941f37c79659b5ebdb7f234f9532fc0783132fc2
Andrew Newton <me@nootn.com.au>
2014-08-23 12:40:40 +1000

    Creates fresh MVC web app from template

---
+++ SampleFluxReactDotNet.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleFluxReactDotNet.Web.MvcApplication" Language="C#" %>
---
+++ SampleFluxReactDotNet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleFluxReactDotNet.Web.Startup))]
+namespace SampleFluxReactDotNet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
